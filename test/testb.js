d59b1ebc07fa459d085e9243d4dbef33405508c6
rjmk <rafekennedy@gmail.com>
2015-06-30 15:50:58 +0100

    adding placeholders

--- test/testb.js
+++ test/testb.js
@@ -3,7 +3,6 @@
 var Code = require('Code');
 var Lab = require('lab');
 var lab = exports.lab = Lab.script();
-var test = require('./test.js');
 
 // some niceties
 
@@ -15,11 +14,17 @@ var expect = Code.expect;
 
 // STOP COPYING HERE
 
+var toCover = require('../toCover');
+
+describe('stop istanbul complaining', function(){
+  it('can be deleted after we have real tests', function(done){
+    expect(toCover());
+  });
+});
+
 describe('strings', function (){
   it('checks that strings have a length', function (done){
     expect('string'.length).to.exist().and.to.be.greaterThan(2);
     done();
   });
 });
-
-module.exports
