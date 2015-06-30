8229dae002a5a4f90579a2c57a89f043513b21c1
Nikhila Ravi <nikkiravi92@gmail.com>
2015-06-30 16:04:08 +0100

    changed handlers

--- routes.js
+++ routes.js
@@ -11,19 +11,21 @@ r.connect( {host: '127.0.0.2', port: 28015, db: 'Facform'}, function(err, conn) 
     connection = conn;
 });
 
-function createUser(user) {
+function createUser(user, reply) {
   r.table('users').insert(user).run(connection, function(err, result) {
     if (err) {throw err;}
-    return JSON.stringify(result, null, 2);
+    // return JSON.stringify(result, null, 2);
+    reply(result);
   });
 }
 
-function readAllUsers() {
+function readAllUsers(reply) {
   r.table('users').run(connection, function(err, cursor) {
       if (err) {throw err;}
       cursor.toArray(function(err, result) {
           if (err) {throw err;}
-          return JSON.stringify(result, null, 2);
+          // return JSON.stringify(result, null, 2);
+          reply(result);
       });
   });
 }
@@ -33,7 +35,7 @@ module.exports = [
     method: "GET",
     path: '/createUser',
     handler: function(request, reply) {
-      reply(createUser({name: "Simon"}));
+      createUser({name: "Fac"}, reply);
     }
 
   },
@@ -41,7 +43,7 @@ module.exports = [
     method: "GET",
     path: '/readUser',
     handler: function(request, reply) {
-      reply(readAllUsers());
+      readAllUsers(reply);
     }
 
   }
