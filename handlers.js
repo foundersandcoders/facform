d2c5cc5cd13a264c8bb5c9cf2fad7ca2c9702fd5
Ronan McCabe <ronan_mccabe@hotmail.com>
2015-07-01 23:22:56 +0100

    auth working

--- handlers.js
+++ handlers.js
@@ -3,21 +3,46 @@ var fs = require('fs');
 
 var handlers = {
   displayLanding: function(request, reply) {
-    if (!request.auth.isAuthenticated) {
-      return reply.view('index');
+    if (request.auth.isAuthenticated) {
+      reply.view('home', { name: request.auth.credentials.profile.displayName });
+    } else {
+      reply.view('home', { name: 'stranger!' });
     }
-      return reply.redirect('/home');
   },
-  login: function(request, reply) {
-    request.auth.session.set(request.auth.credentials);
-    return reply.redirect('/home');
-  },
+
   home: function(request, reply) {
-    if (!request.auth.isAuthenticated) {
-      return reply.view('index');
+    if (request.auth.isAuthenticated) {
+      reply.view('home', { name: request.auth.credentials.profile.displayName });
+    } else {
+      reply.view('home', { name: 'stranger!' });
     }
-      return reply.view('home');
+  },
+
+  login: function(request, reply) {
+    if(request.auth.isAuthenticated) {
+      request.auth.session.set(request.auth.credentials.profile);
+      // console.log(request.auth.credentials);
+      reply.redirect('/');
+      // request.auth.session.set(request.auth.credentials.profile);
+      // reply.view('home', { name: request.auth.credentials.raw.name });
+    } else {
+      reply.view('home', { name: 'stranger!' });
+    }
+  },
+
+  profile: function(request, reply) {
+    if(request.auth.isAuthenticated) {
+      reply.view('profile', { name: request.auth.credentials.profile.displayName });
+    } else {
+      reply.view('home', { name: 'stranger!' });
+    }
+  },
+
+  logoutUser: function(request,reply) {
+    request.auth.session.clear();
+    reply.redirect('/');
   }
+
 };
 
 module.exports = handlers;
