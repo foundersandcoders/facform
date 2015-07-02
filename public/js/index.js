581d90eb52b2cd09e315b47fe1ef81b7f2b63e18
Mina <wilhelmina.jane@gmail.com>
2015-07-02 17:47:25 +0100

    button on lightbox

--- public/js/index.js
+++ public/js/index.js
@@ -4,10 +4,6 @@ document.getElementById("start-button").addEventListener('mousedown', function()
   lightbox.style.display = "inline";
 });
 
-// document.getElementById("no-lightbox").addEventListener('mouseup', function(){
-//   document.getElementById("lightbox").style.display = "none";
-// });
-
 document.getElementById("x").addEventListener('mouseup', function(){
   lightbox.style.display = "none";
   });
