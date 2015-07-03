4c3015f963d0f6e0afd26c3e7ffe6a2473b966e4
Danielwhyte <danwhy@gmail.com>
2015-07-03 15:48:21 +0100

    changed handlers to accept config argument

--- backend/handlers.js
+++ backend/handlers.js
@@ -1,57 +1,58 @@
-var handlebars = require('handlebars');
-var fs = require('fs');
-var DB = require('../rethinkdb.js')();
+module.exports = function(config){
+  var handlebars = require('handlebars');
+  var fs = require('fs');
+  var DB = require('../rethinkdb.js')(config);
 
-var handlers = {
-  displayLanding: function(request, reply) {
-    if (!request.auth.isAuthenticated) {
-      return reply.view('index');
-    }
-      return reply.redirect('/dashboard');
-  },
-  login: function(request, reply) {
-    if(request.auth.isAuthenticated) {
-      request.auth.session.set(request.auth.credentials);
-      DB.create('users', request.auth.credentials.profile, function(data){
-        console.log(request.auth.credentials.profile.displayName + ' added to database');
+  var handlers = {
+    displayLanding: function(request, reply) {
+      if (!request.auth.isAuthenticated) {
+        return reply.view('index');
+      }
+        return reply.redirect('/dashboard');
+    },
+    login: function(request, reply) {
+      if(request.auth.isAuthenticated) {
+        request.auth.session.set(request.auth.credentials);
+        DB.create('users', request.auth.credentials.profile, function(data){
+          console.log(request.auth.credentials.profile.displayName + ' added to database');
+        });
+        reply.view('dashboard', {name: request.auth.credentials.profile.displayName});
+      } else {
+        reply.view('index');
+      }
+    },
+    dashboard: function(request, reply) {
+      if (!request.auth.isAuthenticated) {
+      console.log(request.auth);
+        return reply.view('index');
+      }
+      var context = {
+        name: request.auth.credentials.profile.displayName
+      };
+      return reply.view('dashboard', context);
+    },
+    profile: function(request, reply) {
+      if(request.auth.isAuthenticated) {
+        reply.view('profile', { name: request.auth.credentials.profile.displayName });
+      } else {
+        reply.view('index', { name: 'stranger!' });
+      }
+    },
+    joinChallenge: function(request, reply) {
+      return reply.view('challenge');
+    },
+    logout: function(request, reply) {
+      request.auth.session.clear();
+      return reply.redirect('/');
+    },
+    createSession: function (request,reply){
+      var chatroomNum = request.params.roomNumber;//Submitted from client-side post request;
+      var user1 = request.auth.credentials.profile.username;
+      var kata = request.params.kata;//Submitted from client-side post request;
+      DB.create('activities', {id: chatroomNum, messages:[], participants: [user1], kata: kata},function(){
+        return reply.redirect('/chatbox/'+chatroomNum);
       });
-      reply.view('dashboard', {name: request.auth.credentials.profile.displayName});
-    } else {
-      reply.view('index');
     }
-  },
-  dashboard: function(request, reply) {
-    if (!request.auth.isAuthenticated) {
-    console.log(request.auth);
-      return reply.view('index');
-    }
-    var context = {
-      name: request.auth.credentials.profile.displayName
-    };
-    return reply.view('dashboard', context);
-  },
-  profile: function(request, reply) {
-    if(request.auth.isAuthenticated) {
-      reply.view('profile', { name: request.auth.credentials.profile.displayName });
-    } else {
-      reply.view('index', { name: 'stranger!' });
-    }
-  },
-  joinChallenge: function(request, reply) {
-    return reply.view('challenge');
-  },
-  logout: function(request, reply) {
-    request.auth.session.clear();
-    return reply.redirect('/');
-  },
-  createSession: function (request,reply){
-    var chatroomNum = request.params.roomNumber;//Submitted from client-side post request;
-    var user1 = request.auth.credentials.profile.username;
-    var kata = request.params.kata;//Submitted from client-side post request;
-    DB.create('activities', {id: chatroomNum, messages:[], participants: [user1], kata: kata},function(){
-      return reply.redirect('/chatbox/'+chatroomNum);
-    });
-  }
+  };
+  return handlers;
 };
-
-module.exports = handlers;
